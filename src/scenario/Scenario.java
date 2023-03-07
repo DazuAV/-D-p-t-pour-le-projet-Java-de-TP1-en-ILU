d303d36ba6ad4d8c21c7e968d9da10f45f7a38ec
VRD3647A <VRD3647A@u3-210-10.ens.fsi>
2023-03-07 09:43:21 +0100

    fin de creation scenario

--- src/scenario/Scenario.java
+++ src/scenario/Scenario.java
@@ -7,11 +7,15 @@ public class Scenario {
 
 	public static void main(String[] args) {
 		Gaulois asterix= new Gaulois("Asterix",10);
-		Romain minus= new Romain("Minus",1);
+		Romain minus= new Romain("Minus",2);
 		asterix.parler("Bonjour à tous");
 		minus.parler("UN GAU... UN GAUGAU...");
-		
-
+		asterix.frapper(minus);
+		minus.recevoirCoup(5);
+		asterix.frapper(minus);
+		minus.recevoirCoup(5);
+		asterix.frapper(minus);
+		minus.recevoirCoup(0);
 	}
 
 }
